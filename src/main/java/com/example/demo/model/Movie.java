dcea412c854c59e0095b602a5aba562c3eeaec47
neeju-bash <neerajmohan@gmail.com>
2022-07-12 16:29:54 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/model/Movie.java
@@ -0,0 +1,87 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+public class Movie {
+
+
+    private String id;
+    private String name;
+    private String director;
+    private String rating;
+    private String description;
+    private List<Category> category;
+
+    public Movie() {
+    }
+
+    public Movie(String id, String name, String director, String rating, String description, List<Category> category) {
+        this.id = id;
+        this.name = name;
+        this.director = director;
+        this.rating = rating;
+        this.description = description;
+        this.category = category;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Category> getCategory() {
+        return category;
+    }
+
+    public void setCategory(List<Category> category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", director='" + director + '\'' +
+                ", rating='" + rating + '\'' +
+                ", description='" + description + '\'' +
+                ", category=" + category +
+                '}';
+    }
+}
+
